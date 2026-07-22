445b7359013475f62436850cf3caece78d4d1d67
sandiPC <sandi@tafio.com>
2026-07-22 11:05:34 +0700

    update lembur

--- app/Models/Lembur.php
+++ app/Models/Lembur.php
@@ -31,19 +31,19 @@ class Lembur extends Model
         $bulan = [
             1 => 'januari',
             2 => 'februari',
-            3 => 'maret', 'april',
-            4 => 'mei',
-            5 => 'juni',
-            6 => 'juli',
-            7 => 'agustus',
-            8 => 'september',
-            9 => 'oktober',
-            10 => 'november',
-            11 => 'desember',
+            3 => 'maret',
+            4 => 'april',
+            5 => 'mei',
+            6 => 'juni',
+            7 => 'juli',
+            8 => 'agustus',
+            9 => 'september',
+            10 => 'oktober',
+            11 => 'november',
+            12 => 'desember',
         ];
 
         return $bulan[$this->attributes['bulan']];
-
     }
 
     public function member()
